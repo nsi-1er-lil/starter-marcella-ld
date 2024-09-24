b171c412e053d6ada54fc503c3a97660fa7eec30
marcella-ld <marcellamm@lyceechurchill.london>
2024-09-24 14:57:01 +0100

    Create exercice_4.py

---
+++ Debuter avec Python/exercice_4.py
@@ -0,0 +1,8 @@
+Proposer le code en Python de cet Algorithme dans un fichier au nom exercice_4.py que vous placerez dans ce dossier Debuter avec Python:
+c = float(input())
+P = 4 * c
+A = c ** 2
+b = (A > 5)
+print(P)
+print(A)
+print(b)
