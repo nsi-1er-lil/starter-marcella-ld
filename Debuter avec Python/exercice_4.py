6f7cbf8c330c9fe9b8aaffc45dece91a435ac89f
marcella-ld <marcellamm@lyceechurchill.london>
2024-09-24 14:21:12 +0000

    exercice_4.py

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,8 +1,13 @@
-Proposer le code en Python de cet Algorithme dans un fichier au nom exercice_4.py que vous placerez dans ce dossier Debuter avec Python:
-c = float(input())
+c = 6
 P = 4 * c
 A = c ** 2
 b = (A > 5)
 print(P)
 print(A)
 print(b)
+
+def perimetre(x):
+    return 4 * c
+
+def surface(x):
+    return c * c
